51cfe3ee4f506efb950b7134b75c84af295d306a
Eduardo Doerner <eduardodoerner@gmail.com>
2026-01-15 10:58:27 -0300

    resolve request changes

--- cinema/views.py
+++ cinema/views.py
@@ -1,4 +1,6 @@
+from django.db.models import QuerySet
 from rest_framework import viewsets
+from rest_framework.serializers import Serializer
 
 from cinema.models import (
     Genre,
@@ -38,7 +40,18 @@ class CinemaHallViewSet(viewsets.ModelViewSet):
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
 
-    def get_serializer_class(self):
+    def get_queryset(self) -> QuerySet:
+        queryset = super().get_queryset()
+
+        if self.action == ["list", "retrieve"]:
+            queryset = queryset.prefetch_related(
+                "genres",
+                "actors",
+            )
+
+        return queryset
+
+    def get_serializer_class(self) -> type[Serializer]:
         if self.action == "list":
             return MovieListSerializer
         elif self.action == "retrieve":
@@ -49,7 +62,17 @@ class MovieViewSet(viewsets.ModelViewSet):
 class MovieSessionViewSet(viewsets.ModelViewSet):
     queryset = MovieSession.objects.all()
 
-    def get_serializer_class(self):
+    def get_queryset(self) -> QuerySet:
+        queryset = super().get_queryset()
+        if self.action == ["list", "retrieve"]:
+            queryset = queryset.select_related(
+                "movie",
+                "cinema_hall",
+            )
+
+        return queryset
+
+    def get_serializer_class(self) -> type[Serializer]:
         if self.action == "list":
             return MovieSessionListSerializer
         elif self.action == "retrieve":
